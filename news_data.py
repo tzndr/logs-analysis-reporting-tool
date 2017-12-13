3389877841d900e97fee6bc3d3c320125f8dfe98
Tim Zander <connect@timzander.com>
2017-12-13 16:59:31 -0600

    Add PostgreSQL query for 2nd question

--- news_data.py
+++ news_data.py
@@ -1,3 +1,7 @@
 #Query for 1st problem
 select title, count(title) from articles, log where concat('/article/', articles.slug)
 = log.path group by title order by count desc limit 3;
+
+#Query for 2nd problem
+select name, count(path) from authors, articles, log where authors.id = articles.author
+and concat('/article/', articles.slug) = log.path group by name order by count desc;
