4e20bf299ee72f910c6bd606175bc2c7936debe8
Tim Zander <connect@timzander.com>
2017-12-14 12:41:01 -0600

    Fix typo psycopg2 import

--- news_data.py
+++ news_data.py
@@ -1,39 +1,43 @@
-import psychopg2
+import psycopg2
 
 DBNAME = 'newsdata.sql'
 
 #Fetching results that display the most popular 3 articles of all time
 def top_three_stories():
-    db = psychopg2.Connect(database = DBNAME)
+    db = psycopg2.Connect(database = DBNAME)
     cursor = db.cursor()
-    cursor.execute( "SELECT title, COUNT(title) AS views
+    cursor.execute( """SELECT title, COUNT(title) AS views
                        FROM articles, log
                       WHERE concat('/article/', articles.slug) = log.path
                    GROUP BY title
-                   ORDER BY views desc limit 3;"
+                   ORDER BY views desc limit 3;"""
                    )
     print(cursor.fetchall())
     db.close()
 
 #Fetching results that display the top 3 authors of all time
 def top_authors():
-    db = psychopg2.Connect(database = DBNAME)
+    db = psycopg2.Connect(database = DBNAME)
     cursor = db.cursor()
-    cursor.execute( "  SELECT name, COUNT(path) AS views
+    cursor.execute( """SELECT name, COUNT(path) AS views
                          FROM authors, articles, log where authors.id = articles.author
                           AND concat('/article/', articles.slug) = log.path
                      GROUP BY name
-                     ORDER BY views desc;"
+                     ORDER BY views desc;"""
                    )
     print(cursor.fetchall())
     db.close()
 
 #Fetching results to display which days had more than 1% of requests leading to errors
 def high_errors():
-    db = psychopg2.Connect(database = DBNAME)
+    db = psycopg2.Connect(database = DBNAME)
     cursor = db.cursor()
-    cursor.execute( "  SELECT *
-                       FROM errorlog where "%" > 1;"
+    cursor.execute( """SELECT *
+                         FROM errorlog where "%" > 1;"""
                    )
     print(cursor.fetchall())
     db.close()
+
+top_three_stories()
+top_authors()
+high_errors()
