51660843e11d9f35760d3a4dd3b07e3a22117028
Tim Zander <connect@timzander.com>
2017-12-14 15:56:16 -0600

    Remove newsdata.sql database from repository

--- news_data.py
+++ news_data.py
@@ -4,15 +4,16 @@ import psycopg2
 
 DBNAME = 'news'
 
-#Fetching results that display the most popular 3 articles of all time
+
+# Fetching results that display the most popular 3 articles of all time
 def top_three_stories():
-    db = psycopg2.connect(database = DBNAME)
+    db = psycopg2.connect(database=DBNAME)
     cursor = db.cursor()
-    cursor.execute( """SELECT title, COUNT(title) AS views
-                       FROM articles, log
-                      WHERE concat('/article/', articles.slug) = log.path
-                   GROUP BY title
-                   ORDER BY views desc limit 3;"""
+    cursor.execute("""SELECT title, COUNT(title) AS views
+                        FROM articles, log
+                       WHERE concat('/article/', articles.slug) = log.path
+                    GROUP BY title
+                    ORDER BY views desc limit 3;"""
                    )
     row_1 = cursor.fetchone()
     row_2 = cursor.fetchone()
@@ -22,39 +23,45 @@ def top_three_stories():
     print(' {} - {} views '.format(row_3[0], row_3[1]))
     db.close()
 
-#Fetching results that display the top 3 authors of all time
+
+# Fetching results that display the top 3 authors of all time
 def top_authors():
-    db = psycopg2.connect(database = DBNAME)
+    db = psycopg2.connect(database=DBNAME)
     cursor = db.cursor()
-    cursor.execute( """SELECT name, COUNT(path) AS views
-                         FROM authors, articles, log where authors.id = articles.author
-                          AND concat('/article/', articles.slug) = log.path
-                     GROUP BY name
-                     ORDER BY views desc;"""
+    cursor.execute("""SELECT name, COUNT(path) AS views
+                        FROM authors, articles, log
+                       WHERE authors.id = articles.author
+                         AND concat('/article/', articles.slug) = log.path
+                    GROUP BY name
+                    ORDER BY views desc;"""
                    )
     row = cursor.fetchone()
-    while row != None:
+    while row is not None:
         print(' {} - {} views '.format(row[0], row[1]))
         row = cursor.fetchone()
     db.close()
 
-#Fetching results to display which days had more than 1% of requests leading to errors
+
+# Fetching results to display which days had more than 1% of requests
+# leading to errors
 def high_errors():
-    db = psycopg2.connect(database = DBNAME)
+    db = psycopg2.connect(database=DBNAME)
     cursor = db.cursor()
-    cursor.execute( """SELECT *
-                         FROM errorlog where "%" > 1;"""
+    cursor.execute("""SELECT *
+                        FROM errorlog where "%" > 1;"""
                    )
     row = cursor.fetchone()
-    while row != None:
+    while row is not None:
         print(" {} - {}% errors".format(row[0], row[1]))
         row = cursor.fetchone()
     db.close()
+
 
 print('\n''\n'' The Top 3 Articles of All Time are:''\n')
 top_three_stories()
 print('\n''\n''\n'' The Most Popular Authors of All Time are:''\n')
 top_authors()
-print('\n''\n''\n'' The Days in which More than 1% of Requests Led to Errors:''\n')
+print('\n''\n''\n'' The Days in which More than 1% of Requests Led '
+      'to Errors:''\n')
 high_errors()
 print('\n''\n'' End of Report''\n')
