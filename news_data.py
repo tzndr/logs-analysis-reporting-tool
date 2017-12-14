8fa2aeef68c2b19b53d9fb4b7dde233c5efc5559
Tim Zander <connect@timzander.com>
2017-12-14 12:05:47 -0600

    Add function top_three_stories

--- news_data.py
+++ news_data.py
@@ -1,9 +1,20 @@
-#Query for 1st problem
-   SELECT title, COUNT(title) AS views
-     FROM articles, log
-    WHERE concat('/article/', articles.slug) = log.path
- GROUP BY title
- ORDER views desc limit 3;
+import psychopg2
+
+DBNAME = 'newsdata.sql'
+
+#Fetching results for question 1
+def top_three_stories():
+    db = psychopg2.Connect(database = DBNAME)
+    cursor = db.cursor()
+    cursor.execute( "SELECT title, COUNT(title) AS views
+                       FROM articles, log
+                      WHERE concat('/article/', articles.slug) = log.path
+                   GROUP BY title
+                   ORDER BY views desc limit 3;"
+                   )
+    print(cursor.fetchall())
+    db.close()
+
 
 #Query for 2nd problem
   SELECT name, COUNT(path) AS views
