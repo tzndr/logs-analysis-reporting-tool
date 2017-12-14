08ebd7e618106dc5b5c1a00d2988916ce1b63e87
Tim Zander <connect@timzander.com>
2017-12-14 11:31:54 -0600

    Update query for 2nd question, change count to 'views'

--- news_data.py
+++ news_data.py
@@ -3,8 +3,8 @@ select title, count(title) as views from articles, log where concat('/article/',
 = log.path group by title order by views desc limit 3;
 
 #Query for 2nd problem
-select name, count(path) from authors, articles, log where authors.id = articles.author
-and concat('/article/', articles.slug) = log.path group by name order by count desc;
+select name, count(path) as views from authors, articles, log where authors.id = articles.author
+and concat('/article/', articles.slug) = log.path group by name order by views desc;
 
 #Query for 3rd problem
 select * from errorlog where "%" > 1.0;
